b4fe32bd9cdbc34c7cc991b2afcc24f57f50e285
Connor <Woodconnor13@gmail.com>
2022-09-28 11:10:04 -0400

    palindome solution challenge

---
+++ LeetPalindromeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter a number...");
+int input = int.Parse(Console.ReadLine());
+Console.WriteLine(IsPalindrome(input));
+
+bool IsPalindrome(int x)
+{
+    string xString = x.ToString();
+    string reversedString = "";
+    for (int i = xString.Length - 1; i >= 0; i--)
+    {
+        reversedString += xString[i];
+    }
+    if (reversedString.Contains("-"))
+    {
+        return false;
+    }
+    else
+    {
+        long reversedNumber = long.Parse(reversedString);
+        if (reversedNumber == x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
