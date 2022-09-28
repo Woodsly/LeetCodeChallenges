462dabba32ec045390c87f259e54248f578618ee
Connor <Woodconnor13@gmail.com>
2022-09-28 12:00:35 -0400

    Roman numeral to integer solution

--- LeetPalindromeNumber/Program.cs
+++ LeetPalindromeNumber/Program.cs
@@ -1,20 +1,58 @@
-﻿string[] inputWords = { "flower", "flow", "flight" };
-
-Console.WriteLine(longestCommonPrefix(inputWords));
-
-string longestCommonPrefix(string[] strs)
+﻿string roman = Console.ReadLine();
+Console.WriteLine(RomanToInt(roman));
+int RomanToInt(string s)
 {
-    if (strs.Length == 0)
+    List<int> numbers = new List<int>();
+    foreach (char c in s)
     {
-        return "";
-    }
-    string prefix = strs[0];
-    for (int i = 1; i < strs.Length; i++)
-    {
-        while (strs[i].IndexOf(prefix) != 0)
+        if (c == 'I')
         {
-            prefix = prefix.Substring(0, prefix.Length - 1);
+            numbers.Add(1);
+        }
+        else if (c == 'V')
+        {
+            numbers.Add(5);
+        }
+        else if (c == 'X')
+        {
+            numbers.Add(10);
+        }
+        else if (c == 'L')
+        {
+            numbers.Add(50);
+        }
+        else if (c == 'C')
+        {
+            numbers.Add(100);
+        }
+        else if (c == 'D')
+        {
+            numbers.Add(500);
+        }
+        else if (c == 'M')
+        {
+            numbers.Add(1000);
         }
     }
-    return prefix;
+    int sum = 0;
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        if (i != numbers.Count - 1)
+        {
+            if (numbers[i] < numbers[i + 1])
+            {
+                sum += (numbers[i + 1] - numbers[i]);
+                numbers.RemoveAt(i + 1);
+            }
+            else
+            {
+                sum += numbers[i];
+            }
+        }
+        else
+        {
+            sum += numbers[i];
+        }
+    }
+    return sum;
 }
